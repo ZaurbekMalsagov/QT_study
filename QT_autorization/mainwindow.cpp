e139cf3bac9f62fbfc99e486e1cca61838782804
MenosLord <lordmalsagov@gmail.com>
2024-01-11 15:43:13 +0300

    add resources and image on the authorization_form

--- QT_autorization/mainwindow.cpp
+++ QT_autorization/mainwindow.cpp
@@ -1,12 +1,19 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QMessageBox>
+#include <QPixmap>
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QPixmap pix(":/img/images/png-transparent-customer-login-avatar-head-portrait-client-user.png");
+    int w = ui->image->width();
+    int h = ui->image->height();
+
+    ui->image->setPixmap(pix.scaled(w, h, Qt::KeepAspectRatio));
 }
 
 MainWindow::~MainWindow()
